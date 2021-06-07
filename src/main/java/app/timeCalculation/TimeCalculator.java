4e27a5a13ed52d3adfcd62c4e3a00faec0d0983c
gcfbn <bmekarski@interia.pl>
2021-06-07 16:40:08 +0200

    Code reformatting: remove unused method

--- src/main/java/app/timeCalculation/TimeCalculator.java
+++ src/main/java/app/timeCalculation/TimeCalculator.java
@@ -1,27 +1,11 @@
 package app.timeCalculation;
 
-import app.dto.raw_data.RawWeatherData;
-
 import java.time.Clock;
 import java.time.Instant;
-import java.time.LocalDateTime;
-import java.time.ZoneOffset;
-import java.time.temporal.ChronoUnit;
 
 public class TimeCalculator {
-
-    public static long calculateMinutesFromNow(RawWeatherData rawWeatherData) {
-
-        Instant searchTimeInstant = rawWeatherData.dt();
-        ZoneOffset searchTimeOffset = ZoneOffset.ofTotalSeconds(rawWeatherData.timezone());
-
-        LocalDateTime searchDateTime = LocalDateTime.ofInstant(searchTimeInstant, searchTimeOffset);
-
-        return ChronoUnit.MINUTES.between(searchDateTime, LocalDateTime.now(searchTimeOffset));
-    }
 
     public static long calculateMinutesFromNow(long timestamp) {
         return (Instant.now(Clock.systemUTC()).toEpochMilli() - timestamp * 1000) / 60000;
     }
-
 }
