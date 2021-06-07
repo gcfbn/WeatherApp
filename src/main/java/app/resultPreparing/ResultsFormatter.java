8d057919f9b61aa4bc8aa0fdd51993e648875c20
gcfbn <bmekarski@interia.pl>
2021-06-07 20:16:31 +0200

    Code reformatting: remove commented out code and reformat code

--- src/main/java/app/resultPreparing/ResultsFormatter.java
+++ src/main/java/app/resultPreparing/ResultsFormatter.java
@@ -7,12 +7,6 @@ import java.time.Instant;
 
 public class ResultsFormatter {
 
-//    private final Clouds clouds;
-//    private final Temperature temperature;
-//    private final Info info;
-//    private final Sun sun;
-//    private final Wind wind;
-
     private final RawWeatherData results;
 
     private final String temperatureUnit;
@@ -25,65 +19,65 @@ public class ResultsFormatter {
         this.speedUnit = calculateSpeedUnit(units);
     }
 
-    private static String calculateTemperatureUnit(Units units){
+    private static String calculateTemperatureUnit(Units units) {
         return (units == Units.METRIC) ? "°C" : "°F";
     }
 
-    private static String calculateSpeedUnit(Units units){
+    private static String calculateSpeedUnit(Units units) {
         return (units == Units.METRIC) ? "m/s" : "mph";
     }
 
-    public String currentTemperature(){
+    public String currentTemperature() {
         return results.mainData().temp() + " " + temperatureUnit;
     }
 
-    public String minimumTemperature(){
+    public String minimumTemperature() {
         return results.mainData().tempMin() + " " + temperatureUnit;
     }
 
-    public String maximumTemperature(){
+    public String maximumTemperature() {
         return results.mainData().tempMax() + " " + temperatureUnit;
     }
 
-    public String feelsLike(){
+    public String feelsLike() {
         return results.mainData().tempFeelsLike() + " " + temperatureUnit;
     }
 
-    public String humidity(){
+    public String humidity() {
         return results.mainData().humidity() + " %";
     }
 
-    public String pressure(){
+    public String pressure() {
         return results.mainData().tempFeelsLike() + " hPa";
     }
 
-    public String description(){
+    public String description() {
         return results.weather().get(0).description();
     }
 
-    public String icon(){
+    public String icon() {
         return results.weather().get(0).icon();
     }
 
-    public String windSpeed(){
-        return Double.toString(results.wind().speed());
+    public String windSpeed() {
+        return results.wind().speed() + speedUnit;
     }
 
-    public String windDirection(){
+    public String windDirection() {
         return WindDirectionBuilder.buildDirection(results.wind().deg());
     }
 
-    public String sunrise(){
+    public String sunrise() {
         return (results.sysData().sunrise().equals(Instant.ofEpochSecond(0L))) ?
                 " " : DateBuilder.time(results.sysData().sunrise());
     }
 
-    public String sunset(){
+    public String sunset() {
         return (results.sysData().sunset().equals(Instant.ofEpochSecond(0L))) ?
                 " " : DateBuilder.time(results.sysData().sunset());
     }
 
-    public String overcast(){
+    public String overcast() {
         return results.clouds().all() + " %";
     }
 }
